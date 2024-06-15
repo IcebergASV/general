c01403d4c7986af022ebd3bbfa1c78933141d200
adamm08 <90771946+adamm08@users.noreply.github.com>
2024-06-15 13:04:03 -0230

    added markers_pkg
    Need to do some simple documentation for the package.

---
+++ markers_pkg/launch/launch_file.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='markers_pkg',
+            executable='test_markers',
+            output='screen'),
+    ])
