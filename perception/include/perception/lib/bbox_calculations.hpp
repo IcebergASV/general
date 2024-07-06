ec41e41808a5227daf09fa99c4cf1f28cce34a39
gpearcey <pearcey.grace@gmail.com>
2024-07-06 16:17:03 -0230

    Fix angle calculation

--- perception/include/perception/lib/bbox_calculations.hpp
+++ perception/include/perception/lib/bbox_calculations.hpp
@@ -12,11 +12,12 @@ namespace bbox_calculations
 {
     rclcpp::Logger logger = rclcpp::get_logger("bbox_calculations_logger");
 
-    void getLargestRedAndGreenBoundingBox(const std::vector<perception_interfaces::msg::BoundingBox>& bboxes, perception_interfaces::msg::BoundingBox& left_box, perception_interfaces::msg::BoundingBox& right_box);
+    void getLargestRedAndGreenBoundingBox(const std::vector<perception_interfaces::msg::BoundingBox>& bboxes, perception_interfaces::msg::BoundingBox& red_box, perception_interfaces::msg::BoundingBox& green_box);
     void replaceIfLarger(std::string label, perception_interfaces::msg::BoundingBox& box_to_be_replaced, perception_interfaces::msg::BoundingBox box_to_do_the_replacing);
     double getBoxArea(perception_interfaces::msg::BoundingBox bbox);
     geometry_msgs::msg::Point getBoxCenter(perception_interfaces::msg::BoundingBox bbox);
     void getCamBBoxAngle(perception_interfaces::msg::BoundingBox bbox, double& smaller_angle, double& larger_angle, double camera_fov, double camera_x_res);
     bool includesARedAndGreen(const std::vector<perception_interfaces::msg::BoundingBox>& bboxes);
+    double pixelToAngle(double fov_in_degrees, int res, int pixel);
 }
 #endif // BBOXCALCULATIONS_H
