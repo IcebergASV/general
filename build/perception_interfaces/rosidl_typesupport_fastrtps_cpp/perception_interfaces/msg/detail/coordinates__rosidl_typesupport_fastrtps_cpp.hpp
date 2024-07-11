f847749cc73711296955527c329f00c65200dade
Katie Yanchus <kjyanchus@mun.ca>
2024-07-11 20:49:30 -0230

    .

---
+++ build/perception_interfaces/rosidl_typesupport_fastrtps_cpp/perception_interfaces/msg/detail/coordinates__rosidl_typesupport_fastrtps_cpp.hpp
@@ -0,0 +1,80 @@
+// generated from rosidl_typesupport_fastrtps_cpp/resource/idl__rosidl_typesupport_fastrtps_cpp.hpp.em
+// with input from perception_interfaces:msg/Coordinates.idl
+// generated code does not contain a copyright notice
+
+#ifndef PERCEPTION_INTERFACES__MSG__DETAIL__COORDINATES__ROSIDL_TYPESUPPORT_FASTRTPS_CPP_HPP_
+#define PERCEPTION_INTERFACES__MSG__DETAIL__COORDINATES__ROSIDL_TYPESUPPORT_FASTRTPS_CPP_HPP_
+
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "perception_interfaces/msg/rosidl_typesupport_fastrtps_cpp__visibility_control.h"
+#include "perception_interfaces/msg/detail/coordinates__struct.hpp"
+
+#ifndef _WIN32
+# pragma GCC diagnostic push
+# pragma GCC diagnostic ignored "-Wunused-parameter"
+# ifdef __clang__
+#  pragma clang diagnostic ignored "-Wdeprecated-register"
+#  pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
+# endif
+#endif
+#ifndef _WIN32
+# pragma GCC diagnostic pop
+#endif
+
+#include "fastcdr/Cdr.h"
+
+namespace perception_interfaces
+{
+
+namespace msg
+{
+
+namespace typesupport_fastrtps_cpp
+{
+
+bool
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_perception_interfaces
+cdr_serialize(
+  const perception_interfaces::msg::Coordinates & ros_message,
+  eprosima::fastcdr::Cdr & cdr);
+
+bool
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_perception_interfaces
+cdr_deserialize(
+  eprosima::fastcdr::Cdr & cdr,
+  perception_interfaces::msg::Coordinates & ros_message);
+
+size_t
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_perception_interfaces
+get_serialized_size(
+  const perception_interfaces::msg::Coordinates & ros_message,
+  size_t current_alignment);
+
+size_t
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_perception_interfaces
+max_serialized_size_Coordinates(
+  bool & full_bounded,
+  bool & is_plain,
+  size_t current_alignment);
+
+}  // namespace typesupport_fastrtps_cpp
+
+}  // namespace msg
+
+}  // namespace perception_interfaces
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+ROSIDL_TYPESUPPORT_FASTRTPS_CPP_PUBLIC_perception_interfaces
+const rosidl_message_type_support_t *
+  ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_fastrtps_cpp, perception_interfaces, msg, Coordinates)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // PERCEPTION_INTERFACES__MSG__DETAIL__COORDINATES__ROSIDL_TYPESUPPORT_FASTRTPS_CPP_HPP_
