d76f3644e0a8856378a5e29e1ecaf4c6f5f474c1
Michael McCarthy <mjmccarthy@mun.ca>
2024-07-27 12:24:28 -0230

    cleaned up wp_sender

--- vehicle_control/src/wp_sender.cpp
+++ vehicle_control/src/wp_sender.cpp
@@ -46,7 +46,7 @@ class WaypointSender : public rclcpp::Node
         {
           RCLCPP_INFO(this->get_logger(), "Guided mode: true");
           in_guided_ = true;
-          publishStraightAheadWaypoint();
+          publish_local_waypoint();
         }
         else
         {
@@ -57,7 +57,7 @@ class WaypointSender : public rclcpp::Node
 
     void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
     {
-      current_local_position_ = *msg;
+      local_position = *msg;
     }
 
     void wpReachedCallback(const mavros_msgs::msg::WaypointReached msg)
@@ -66,7 +66,7 @@ class WaypointSender : public rclcpp::Node
 
       if (wp_reached_counter < wp_reached_max_count && msg.wp_seq == 0)
       {
-        publishStraightAheadWaypoint();
+        publish_local_waypoint();
         wp_reached_counter++;
       }
       else
@@ -75,97 +75,26 @@ class WaypointSender : public rclcpp::Node
       }
     }
 
-    geometry_msgs::msg::PoseStamped calculateWaypointStraightAhead()
+    void publish_local_waypoint()
     {
 
-        // Create a new waypoint
-        geometry_msgs::msg::PoseStamped local_position; 
-        local_position.header = current_local_position_.header;
-        double x = 3;
-        double y = -3;
-        local_position.pose.position.x = y;
-        local_position.pose.position.y = -x;
-
-        tf2::Quaternion orientation;
-        tf2::fromMsg(current_local_position_.pose.orientation, orientation);
-        double roll, pitch, yaw;
-        tf2::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
-
-        local_position.pose.orientation = tf2::toMsg(tf2::Quaternion(0, 0, yaw, 1));
-        geometry_msgs::msg::PoseStamped waypoint = local_to_relative_grace(local_position, current_local_position_ );
-        //geometry_msgs::msg::PoseStamped waypoint = local_to_relative(local_position, current_local_position_);
-      //RCLCPP_INFO(this->get_logger(), "Current position: x=%f, y=%f, z=%f", current_local_position_.pose.position.x, current_local_position_.pose.position.y, current_local_position_.pose.position.z);
-      //RCLCPP_INFO(this->get_logger(), "Waypoint position: x=%f, y=%f, z=%f", waypoint.pose.position.x, waypoint.pose.position.y, waypoint.pose.position.z);
-
-      return waypoint;
+      // Create a new waypoint
+      geometry_msgs::msg::PoseStamped relative_position; 
+      relative_position.header = local_position.header;
+      double x = 3;
+      double y = -3;
+      relative_position.pose.position.x = y;
+      relative_position.pose.position.y = -x;
+      
+      geometry_msgs::msg::PoseStamped waypoint = rel_to_local_cord_converter(relative_position, local_position );
+      RCLCPP_INFO(this->get_logger(), "Current Local position: x=%f, y=%f, z=%f", local_position.pose.position.x, local_position.pose.position.y, local_position.pose.position.z);
+      RCLCPP_INFO(this->get_logger(), "Waypoint position: x=%f, y=%f, z=%f", waypoint.pose.position.x, waypoint.pose.position.y, waypoint.pose.position.z);
+      
+      waypoint_publisher->publish(waypoint);
     }
 
-    void publishStraightAheadWaypoint()
+    geometry_msgs::msg::PoseStamped rel_to_local_cord_converter(const geometry_msgs::msg::PoseStamped &rel_pose, const geometry_msgs::msg::PoseStamped &local_pose)
     {
-      //publish waypoint
-      waypoint_publisher->publish(calculateWaypointStraightAhead());
-      //calculateWaypointStraightAhead();
-    }
-
-    geometry_msgs::msg::PoseStamped local_to_relative(
-        const geometry_msgs::msg::PoseStamped &local_position,
-        const geometry_msgs::msg::PoseStamped &robot_pose)
-    {
-        // Extract the robot's position and orientation
-        const auto &robot_position = robot_pose.pose.position;
-        const auto &robot_orientation = robot_pose.pose.orientation;
-
-        // Convert the robot's quaternion to a rotation matrix
-        tf2::Quaternion quat(robot_orientation.x, robot_orientation.y, robot_orientation.z, robot_orientation.w);
-        tf2::Matrix3x3 rotation_matrix;
-        quat.normalize();
-        rotation_matrix.setRotation(quat);
-
-        // Convert rotation matrix to Eigen matrix for easier manipulation
-        Eigen::Matrix3d eigen_rotation_matrix;
-        for (int i = 0; i < 3; ++i)
-            for (int j = 0; j < 3; ++j)
-                eigen_rotation_matrix(i, j) = rotation_matrix[i][j];
-        
-        RCLCPP_INFO(this->get_logger(), "Rotation Matrix");
-        printMatrix(eigen_rotation_matrix);
-
-        // Convert local position to Eigen vector
-        Eigen::Vector3d local_position_vec(
-            local_position.pose.position.x,
-            local_position.pose.position.y,
-            local_position.pose.position.z);
-
-        RCLCPP_INFO(this->get_logger(), "local position vec position: %f, y=%f, z=%f", local_position_vec.x(), local_position_vec.y(), local_position_vec.z());
-
-        // Convert robot position to Eigen vector
-        Eigen::Vector3d robot_position_vec(
-            robot_position.x,
-            robot_position.y,
-            robot_position.z);
-
-         RCLCPP_INFO(this->get_logger(), "robot position vec position: x=%f, y=%f, z=%f", robot_position_vec.x(), robot_position_vec.y(), robot_position_vec.z());
-
-        // Compute the relative position
-        Eigen::Vector3d relative_position_vec = eigen_rotation_matrix.transpose() * (local_position_vec - robot_position_vec);
-
-        // Create PoseStamped message for the relative position
-        geometry_msgs::msg::PoseStamped relative_position;
-        relative_position.header.frame_id = "base_link";  // Frame of reference for the robot
-        relative_position.pose.position.x = relative_position_vec.x();
-        relative_position.pose.position.y = relative_position_vec.y();
-        relative_position.pose.position.z = relative_position_vec.z();
-        relative_position.pose.orientation = robot_pose.pose.orientation;  // Orientation remains unchanged
-
-        return relative_position;
-    }
-
-    geometry_msgs::msg::PoseStamped local_to_relative_grace(const geometry_msgs::msg::PoseStamped &rel_pose, const geometry_msgs::msg::PoseStamped &local_pose)
-    {
-      // radius = std::sqrt(rel_pose.pose.position.y * rel_pose.pose.position.y + rel_pose.pose.position.x * rel_pose.pose.position.x);
-      //double angle = std::atan2(rel_pose.pose.position.y, rel_pose.pose.position.x);
-      //RCLCPP_INFO(this->get_logger(), "radius: %f, angle: %f", radius, angle);
-
       // Convert relative coordinates to local coordinates
       tf2::Quaternion orientation;
       tf2::fromMsg(local_pose.pose.orientation, orientation);
@@ -194,17 +123,6 @@ class WaypointSender : public rclcpp::Node
       return relative_position;
     }
 
-    void printMatrix(const Eigen::Matrix3d &matrix)
-    {
-        std::ostringstream oss;
-        oss << "Rotation Matrix:\n" 
-            << matrix(0, 0) << " " << matrix(0, 1) << " " << matrix(0, 2) << "\n"
-            << matrix(1, 0) << " " << matrix(1, 1) << " " << matrix(1, 2) << "\n"
-            << matrix(2, 0) << " " << matrix(2, 1) << " " << matrix(2, 2) << "\n";
-
-        RCLCPP_INFO(this->get_logger(), "%s", oss.str().c_str());
-    }
-
     rclcpp::Subscription<mavros_msgs::msg::State>::SharedPtr state_subscriber_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscriber_;
     rclcpp::Subscription<mavros_msgs::msg::WaypointReached>::SharedPtr wp_reached_subscriber;
@@ -216,7 +134,7 @@ class WaypointSender : public rclcpp::Node
     int wp_reached_counter;
     int wp_reached_max_count;
 
-    geometry_msgs::msg::PoseStamped current_local_position_;
+    geometry_msgs::msg::PoseStamped local_position;
 };
 
 int main(int argc, char * argv[])
