2aa89e7988eac70cdd8de138d3612c26719106a2
Jordyn <jelizabethob@mun.ca>
2024-08-12 08:53:47 -0230

    Task logic

--- njord_tasks/include/njord_tasks/navigation_component.hpp
+++ njord_tasks/include/njord_tasks/navigation_component.hpp
@@ -23,19 +23,32 @@ public:
 private:
     void callback(const njord_tasks_interfaces::msg::StartTask::SharedPtr msg);
     void timerCallback();
+    void wpReachedCallback(const std_msgs::msg::Int32::SharedPtr msg);
 
     rclcpp::TimerBase::SharedPtr timer_;
     rcl_interfaces::msg::SetParametersResult param_callback(const std::vector<rclcpp::Parameter> &params);
 
     rclcpp::Subscription<njord_tasks_interfaces::msg::StartTask>::SharedPtr example_sub_;
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr example_pub_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr wp_reached_sub_;
     rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr on_set_parameters_callback_handle_;
+
+    bool wp_reached_;
 
     enum States {WAIT_FOR_START, START_TASK, GOING_TO_WAYPOINT, REACHED_WAYPOINT, GOING_TO_FINISH, COMPLETE}; 
     States status_ = States::WAIT_FOR_START;
 
-    geographic_msgs::msg::GeoPoseStamped next_point;
-    std::vector<std::pair<double, double>> test;
+    double start_lat;
+    double start_lon;
+
+    double finish_lat;
+    double finish_lon;
+
+    geographic_msgs::msg::GeoPoseStamped current_point;
+    std::vector<double> point_list_1D;
+    std::vector<std::vector<double>> point_list_2D;
+
+    int32_t number_of_points;
+    int32_t index_of_current_point;
 
     template <typename T>
     void getParam(std::string param_name, T& param, T default_value, std::string desc)
@@ -44,11 +57,27 @@ private:
       param_desc.description = desc;
       this->declare_parameter<T>(param_name, default_value, param_desc);
       this->get_parameter(param_name, param);
-      std::string param_log_output = param_name + ": " + std::to_string(param);
-      RCLCPP_INFO(this->get_logger(), param_log_output.c_str()); 
+      // std::string param_log_output = param_name + ": " + std::to_string(param);
+      //RCLCPP_INFO(this->get_logger(), param_log_output.c_str()); 
 
       return;
     }
+
+    std::vector<std::vector<double>> convertTo2DList(const std::vector<double>& input_list) {
+      std::vector<std::vector<double>> output_list;
+
+      // Check if the input list has an even number of elements
+      if (input_list.size() % 2 != 0) {
+          throw std::invalid_argument("Input list must contain an even number of elements.");
+      }
+
+      for (size_t i = 0; i < input_list.size(); i += 2) {
+          output_list.push_back({input_list[i], input_list[i + 1]});
+      }
+
+    return output_list;
+    }
+
 };
 
 } // namespace njord_tasks
--- njord_tasks/src/navigation_component.cpp
+++ njord_tasks/src/navigation_component.cpp
@@ -7,78 +7,112 @@ namespace njord_tasks
   : Node("navigation", options)
   {
     example_sub_ = this->create_subscription<njord_tasks_interfaces::msg::StartTask>("/njord_tasks/task_to_execute", 10, std::bind(&navigation::callback, this, _1));
+    wp_reached_sub_ = this->create_subscription<std_msgs::msg::Int32>("/njord_tasks/wp_reached_test", 10, std::bind(&navigation::wpReachedCallback, this, _1));
     timer_ = this->create_wall_timer(500ms, std::bind(&navigation::timerCallback, this));
-    navigation::getParam<std::vector<std::pair<double, double>>>("nav_points", test, {{0.0,0.0}}, "Nav points");
     
+    navigation::getParam("nav_points", point_list_1D, std::vector<double>{}, "List of 2D points");
+    point_list_2D = navigation::convertTo2DList(point_list_1D);
+
+    number_of_points = static_cast<int32_t>(point_list_2D.size());
+    index_of_current_point = 0;
+
+
   }
 
   void navigation::callback(const njord_tasks_interfaces::msg::StartTask::SharedPtr msg)
   {
        if(msg->task.current_task == njord_tasks_interfaces::msg::Task::NAVIGATION){
         status_ = States::START_TASK;
+        start_lat = msg->start_pnt.latitude;
+        start_lon = msg->start_pnt.longitude;
+        finish_lat = msg->finish_pnt.latitude;
+        finish_lon = msg->finish_pnt.longitude;
        }
+  }
+
+  void navigation::wpReachedCallback(const std_msgs::msg::Int32::SharedPtr msg)
+  {
+    RCLCPP_INFO(this->get_logger(), "Waypoint Reached");
+    //mavros_msgs::msg::WaypointReached wpr = msg;
+    wp_reached_ = true;
   }
 
   void navigation::timerCallback(){
     switch (status_){
       case States::WAIT_FOR_START:
       {
-        RCLCPP_INFO(this->get_logger(), "Waiting for task to start");
+        RCLCPP_DEBUG(this->get_logger(), "Waiting for task to start");
         break;
 
       }
       case States::START_TASK:
       {
-        RCLCPP_INFO(this->get_logger(), "Sending first waypoint");
 
-        // Set way point to first 
-        //publish waypoint
+        RCLCPP_INFO(this->get_logger(), "Starting navigation task");
+        RCLCPP_INFO(this->get_logger(), "point: %.2d", number_of_points);
+
+        current_point = task_lib::translate_lat_lon(start_lat, start_lon, point_list_2D[index_of_current_point][0], point_list_2D[index_of_current_point][1] );
         
+        RCLCPP_INFO(this->get_logger(), "Publishing x: %.4f, y: %.4f", point_list_2D[index_of_current_point][0], point_list_2D[index_of_current_point][1]);
+        RCLCPP_INFO(this->get_logger(), "GeoPoseStamped created with latitude: %.8f, longitude: %.8f", 
+                current_point.pose.position.latitude, current_point.pose.position.longitude);
+        wp_reached_ = false;
         status_ = GOING_TO_WAYPOINT;
         break;
       }
       case States::GOING_TO_WAYPOINT:
       {
-
-        // if waypoint reached 
-            //if last waypoint reached 
-                // next state = going to finish 
-
-            //else
-                //next state = waypoint reached 
-
-
-        geographic_msgs::msg::GeoPoseStamped next_point = task_lib::translate_lat_lon(45, 50, 2000, 3000);
-
-        RCLCPP_INFO(this->get_logger(), "GeoPoseStamped created with latitude: %.8f, longitude: %.8f", 
-                next_point.pose.position.latitude, next_point.pose.position.longitude);
-
+        if(wp_reached_){
+          
+          index_of_current_point += 1;
+          if(index_of_current_point == number_of_points){
+            RCLCPP_INFO(this->get_logger(), "GeoPoseStamped created with latitude: %.8f, longitude: %.8f", 
+                finish_lat, finish_lon);
+            status_ = GOING_TO_FINISH;
+            break;
+          }
+          else{
+            status_ = REACHED_WAYPOINT;
+            break;
+          }
+        }
+        status_ = GOING_TO_WAYPOINT;
+        break;
       }
       case States::REACHED_WAYPOINT:
       {
+        RCLCPP_INFO(this->get_logger(), "Reached waypoint");
+        wp_reached_ = false;
+        current_point = task_lib::translate_lat_lon(start_lat, start_lon, point_list_2D[index_of_current_point][0], point_list_2D[index_of_current_point][1]);
 
-        // send next waypoint
+        RCLCPP_INFO(this->get_logger(), "Publishing x: %.4f, y: %.4f", point_list_2D[index_of_current_point][0], point_list_2D[index_of_current_point][1]);
+        RCLCPP_INFO(this->get_logger(), "GeoPoseStamped created with latitude: %.8f, longitude: %.8f", 
+                current_point.pose.position.latitude, current_point.pose.position.longitude);
+        
+        status_ = GOING_TO_WAYPOINT;
+        break;
 
       }
 
       case States::GOING_TO_FINISH:
       {
+        RCLCPP_INFO(this->get_logger(), "Going to finish");
 
-
+        if(wp_reached_){
+          wp_reached_ = false;
+          status_ = COMPLETE;
+          break;
+        }
+        status_ = GOING_TO_FINISH;
+        break;
       }
 
-      case States::COMPLETE
+      case States::COMPLETE:
       {
-
-
+        RCLCPP_INFO(this->get_logger(), "Complete!!!!!!!");
       }
-      
-
 
     }
-
-
-
   }
   
 }
