abfb616867ae8d507668aaa3c4388bc447325a3f
gpearcey <pearcey.grace@gmail.com>
2024-08-13 19:29:18 +0200

    Fixing obstacle vizualization

--- njord_tasks/src/collision_avoidance_component.cpp
+++ njord_tasks/src/collision_avoidance_component.cpp
@@ -207,13 +207,13 @@ visualization_msgs::msg::MarkerArray CollisionAvoidance::create_segment_viz_poin
 
   // Create a deletion marker to clear the previous points
   visualization_msgs::msg::Marker deletion_marker;
-  //deletion_marker.header = header;
+  deletion_marker.header.frame_id = "base_scan";
   deletion_marker.action = visualization_msgs::msg::Marker::DELETEALL;
   viz_array.markers.push_back(deletion_marker);
 
   // Create a marker point
   visualization_msgs::msg::Marker viz_points;
-  //viz_points.header = header;
+  viz_points.header.frame_id = "base_scan";
   viz_points.lifetime = rclcpp::Duration(0, 10);
   viz_points.ns = "segments";
   viz_points.type = visualization_msgs::msg::Marker::POINTS;
@@ -222,7 +222,7 @@ visualization_msgs::msg::MarkerArray CollisionAvoidance::create_segment_viz_poin
 
   // Create a marker centroid
   visualization_msgs::msg::Marker viz_centroids;
-  //viz_centroids.header = header;
+  viz_centroids.header.frame_id = "base_scan";
   viz_centroids.lifetime = rclcpp::Duration(0, 10);
   viz_centroids.ns = "centroids";
   viz_centroids.type = visualization_msgs::msg::Marker::CUBE;
@@ -231,7 +231,7 @@ visualization_msgs::msg::MarkerArray CollisionAvoidance::create_segment_viz_poin
 
   // Create a marker id text
   visualization_msgs::msg::Marker viz_text;
-  //viz_text.header = header;
+  viz_text.header.frame_id = "base_scan";
   viz_text.lifetime = rclcpp::Duration(0, 10);
   viz_text.ns = "id";
   viz_text.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
@@ -250,7 +250,7 @@ visualization_msgs::msg::MarkerArray CollisionAvoidance::create_segment_viz_poin
 
     // Change the color of the segment
     std_msgs::msg::ColorRGBA color;
-    color.r = 250; color.g = 237; color.b = 49;
+    color.r = 0.90; color.g = 0.8; color.b = 0.2; color.a = 1.0;
     viz_points.color = color;
     viz_centroids.color = color;
 
