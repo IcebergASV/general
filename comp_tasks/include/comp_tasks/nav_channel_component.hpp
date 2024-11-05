c23385137c437f9d7275f3a746a869abe21f6d3c
gpearcey <pearcey.grace@gmail.com>
2024-11-05 16:25:14 -0330

    Add nav channel sub class

---
+++ comp_tasks/include/comp_tasks/nav_channel_component.hpp
@@ -0,0 +1,46 @@
+#ifndef nav_channel_HPP
+#define nav_channel_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <comp_tasks/maneuvering_component.hpp>
+#include <std_msgs/msg/int32.hpp>
+#include <std_msgs/msg/float64.hpp>
+
+using std::placeholders::_1;
+
+namespace comp_tasks
+{
+
+class NavChannel : public comp_tasks::Maneuvering
+{
+public:
+    explicit NavChannel(const rclcpp::NodeOptions & options);
+
+private:
+    void callback(const std_msgs::msg::Int32::SharedPtr msg);
+    rcl_interfaces::msg::SetParametersResult param_callback(const std::vector<rclcpp::Parameter> &params);
+
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr example_sub_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr example_pub_;
+    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr on_set_parameters_callback_handle_;
+
+    int p_multiplier_;
+    double p_adder_;
+
+    template <typename T>
+    void getParam(std::string param_name, T& param, T default_value, std::string desc) override
+    {
+      auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+      param_desc.description = desc;
+      this->declare_parameter<T>(param_name, default_value, param_desc);
+      this->get_parameter(param_name, param);
+      std::string param_log_output = param_name + ": " + std::to_string(param);
+      RCLCPP_INFO(this->get_logger(), param_log_output.c_str()); 
+
+      return;
+    }
+};
+
+} // namespace comp_tasks
+
+#endif // nav_channel_HPP
