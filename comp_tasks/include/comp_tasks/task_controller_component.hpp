6b29b23dd860d7a598fb46c111bf255e795836e7
gpearcey <pearcey.grace@gmail.com>
2025-01-09 11:40:28 -0330

    Add task controller to update lifecycle nodes

---
+++ comp_tasks/include/comp_tasks/task_controller_component.hpp
@@ -0,0 +1,176 @@
+#ifndef task_controller_HPP
+#define task_controller_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32.hpp>
+#include <std_msgs/msg/float64.hpp>
+#include "lifecycle_msgs/msg/state.hpp"
+#include "lifecycle_msgs/msg/transition.hpp"
+#include "lifecycle_msgs/srv/change_state.hpp"
+#include "lifecycle_msgs/srv/get_state.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+namespace comp_tasks
+{
+  template<typename FutureT, typename WaitTimeT>
+  std::future_status
+  wait_for_result(
+    FutureT & future,
+    WaitTimeT time_to_wait)
+  {
+    auto end = std::chrono::steady_clock::now() + time_to_wait;
+    std::chrono::milliseconds wait_period(100);
+    std::future_status status = std::future_status::timeout;
+    do {
+      auto now = std::chrono::steady_clock::now();
+      auto time_left = end - now;
+      if (time_left <= std::chrono::seconds(0)) {break;}
+      status = future.wait_for((time_left < wait_period) ? time_left : wait_period);
+    } while (rclcpp::ok() && status != std::future_status::ready);
+    return status;
+  }
+
+class TaskController : public rclcpp::Node
+{
+public:
+    explicit TaskController(const rclcpp::NodeOptions & options);
+    void init();
+    unsigned int get_state(std::chrono::seconds time_out = 3s);
+    bool change_state(std::uint8_t transition, std::chrono::seconds time_out = 3s);
+private:
+    void callback(const std_msgs::msg::Int32::SharedPtr msg);
+    rcl_interfaces::msg::SetParametersResult param_callback(const std::vector<rclcpp::Parameter> &params);
+
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr example_sub_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr example_pub_;
+    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr on_set_parameters_callback_handle_;
+
+    int p_multiplier_;
+    double p_adder_;
+
+    static constexpr char const * lifecycle_node = "test_life_cycle_node";
+    static constexpr char const * node_get_state_topic = "test_life_cycle_node/get_state";
+    static constexpr char const * node_change_state_topic = "test_life_cycle_node/change_state";
+    std::shared_ptr<rclcpp::Client<lifecycle_msgs::srv::GetState>> client_get_state_;
+    std::shared_ptr<rclcpp::Client<lifecycle_msgs::srv::ChangeState>> client_change_state_;
+    template <typename T>
+    void getParam(std::string param_name, T& param, T default_value, std::string desc)
+    {
+      auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+      param_desc.description = desc;
+      this->declare_parameter<T>(param_name, default_value, param_desc);
+      this->get_parameter(param_name, param);
+      std::string param_log_output = param_name + ": " + std::to_string(param);
+      RCLCPP_INFO(this->get_logger(), param_log_output.c_str()); 
+
+      return;
+    }
+};
+
+void
+callee_script(std::shared_ptr<TaskController> lc_client)
+{
+  rclcpp::WallRate time_between_state_changes(0.1);  // 10s
+
+  // configure
+  {
+    if (!lc_client->change_state(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE)) {
+      return;
+    }
+    if (!lc_client->get_state()) {
+      return;
+    }
+  }
+
+  // activate
+  {
+    time_between_state_changes.sleep();
+    if (!rclcpp::ok()) {
+      return;
+    }
+    if (!lc_client->change_state(lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE)) {
+      return;
+    }
+    if (!lc_client->get_state()) {
+      return;
+    }
+  }
+
+  // deactivate
+  {
+    time_between_state_changes.sleep();
+    if (!rclcpp::ok()) {
+      return;
+    }
+    if (!lc_client->change_state(lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE)) {
+      return;
+    }
+    if (!lc_client->get_state()) {
+      return;
+    }
+  }
+
+  // activate it again
+  {
+    time_between_state_changes.sleep();
+    if (!rclcpp::ok()) {
+      return;
+    }
+    if (!lc_client->change_state(lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE)) {
+      return;
+    }
+    if (!lc_client->get_state()) {
+      return;
+    }
+  }
+
+  // and deactivate it again
+  {
+    time_between_state_changes.sleep();
+    if (!rclcpp::ok()) {
+      return;
+    }
+    if (!lc_client->change_state(lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE)) {
+      return;
+    }
+    if (!lc_client->get_state()) {
+      return;
+    }
+  }
+
+  // we cleanup
+  {
+    time_between_state_changes.sleep();
+    if (!rclcpp::ok()) {
+      return;
+    }
+    if (!lc_client->change_state(lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP)) {
+      return;
+    }
+    if (!lc_client->get_state()) {
+      return;
+    }
+  }
+
+  // and finally shutdown
+  // Note: We have to be precise here on which shutdown transition id to call
+  // We are currently in the unconfigured state and thus have to call
+  // TRANSITION_UNCONFIGURED_SHUTDOWN
+  {
+    time_between_state_changes.sleep();
+    if (!rclcpp::ok()) {
+      return;
+    }
+    if (!lc_client->change_state(lifecycle_msgs::msg::Transition::TRANSITION_UNCONFIGURED_SHUTDOWN))
+    {
+      return;
+    }
+    if (!lc_client->get_state()) {
+      return;
+    }
+  }
+}
+} // namespace comp_tasks
+
+#endif // task_controller_HPP
